d86ca92f595dd9813c5407dfca343028460f0e39
Malik Hesham <malikhesham@outlook.com>
2021-07-02 00:31:42 +0200

    cleaning up the un used imports and methods

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -4,20 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\TodoValidationRequest;
 use App\Models\Todo;
-use App\Models\User;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
 class TodoController extends Controller {
 
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create() {
-    }
 
     /**
      * Store a newly created resource in storage.
@@ -32,27 +22,7 @@ class TodoController extends Controller {
         Todo::create($request->all());
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Todo $todo)
-    {
-        //
-    }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Todo $todo)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
@@ -66,15 +36,5 @@ class TodoController extends Controller {
         $updatedTodo = Todo::find($todo);
         $updatedTodo->update($request->all());
         $updatedTodo->save();
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Models\Todo  $todo
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($todoId){
-
     }
 }
